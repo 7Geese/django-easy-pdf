8f4db91ddd5e89fbf215e66cb18ff9acd9978c71
Carlos Galindo <carlos@7geese.com>
2025-12-16 17:21:37 -0600

    bump version in setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = "0.1.2.dev1"
+version = "0.1.2.dev2"
 
 if sys.argv[-1] == "publish":
     os.system("python setup.py sdist bdist_wheel upload")
